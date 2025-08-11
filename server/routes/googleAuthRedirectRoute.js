ad4b6792c465b3b0e5626c15a9cbffde70cdecc7
akashsaini3032 <akashsainira@gmail.com>
2025-08-11 14:03:04 +0530

    update

--- server/routes/googleAuthRedirectRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getGoogleAuthURL, googleCallback } = require('../controllers/googleAuthRedirectController');
-
-router.get('/google', getGoogleAuthURL);
-router.get('/google/callback', googleCallback);
-
-module.exports = router;
